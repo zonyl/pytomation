cdad31c0fab07b149de33b36e41c104488793930
Jason Sharpee <admin@pytomation.com>
2012-05-19 22:06:47 -0400

    Just getting the test environment setup for TDD

---
+++ tests/message.py
@@ -0,0 +1,13 @@
+from unittest import TestCase, main
+
+
+class MessageTests(TestCase):
+
+	def setUp(self):
+		pass
+
+	def message_setup_test(self):
+		pass
+
+if __name__ == '__main__':
+    main()
