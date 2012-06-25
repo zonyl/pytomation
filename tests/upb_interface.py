bf56c79005de4748085626aa4c3f07fc8a42746b
Jason <jason@sharpee.com>
2012-06-24 20:50:32 -0400

    Use a mocked Serial Port for unit test run.  It actually works now!

--- tests/upb_interface.py
+++ tests/upb_interface.py
@@ -17,10 +17,42 @@ class UPBInterfaceTests(TestCase):
                              'UPB interface could not be instantiated')
 
     def test_get_firmware_version(self):
-#        self.assertIsNone(self.upb.command('29', HACommand.ON))
-        self.upb.get_register()
+        useMock = True
+        if useMock:  # Use Mock Serial Port
+            ms = MockSerial()
+            ms._response = 'PR021234\x0D'
+            self.upb = None
+            self.upb = UPB(ms)
+            self.upb.start()
+
+        response = self.upb.get_register(2, 2)
+        self.assertEqual(response, '1234')
+        if useMock:
+            self.assertEqual(ms._written, '\x120202FC\x0D')
         #sit and spin, let the magic happen
-        select.select([], [], [])
+        #select.select([], [], [])
+
+
+class MockSerial(object):
+    _written = None
+    _response = None
+    _response_q = None
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def write(self, data=None):
+        self._written = data
+        self._response_q = self._response
+
+    def read(self, count=None):
+        response = ""
+        if self._response_q:
+            if not count:
+                count = len(self._response_q)
+                response = self._response_q[:count]
+                self._response_q = None
+        return response
 
 if __name__ == '__main__':
     main()
