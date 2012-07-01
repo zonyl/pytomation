37e63b94251aee51772d06ae7b975fd1e942fec3
Jason <jason@sharpee.com>
2012-07-01 07:57:39 -0400

    Example use code.

---
+++ example_use.py
@@ -0,0 +1,20 @@
+from pytomation.interfaces import UPB, Serial
+
+serial = Serial('/dev/ttyUSB0', 4800)
+upb = UPB(serial)
+upb.start()
+
+# Turn on Light - Network: 49, ID: 3
+response = upb.on((49, 3))
+
+# Turn off Light - Network: 49, ID: 3
+response2 = upb.off((49, 3))
+
+# Check for success
+if response:
+    print "Message was successfully sent!"
+else:
+    print "Interface not responding"
+
+# Code is down, we no longer need the interface
+upb.shutdown()
