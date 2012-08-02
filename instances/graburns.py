f3c9f34e3fa67d9d3b70f4a1bd7e319a7f0ffe5b
Jason <jason@sharpee.com>
2012-08-01 21:38:01 -0400

    Create a new instance of automation at my home.

---
+++ instances/graburns.py
@@ -0,0 +1,35 @@
+from pytomation.interfaces import UPB, InsteonPLM, TCP, Serial, Stargate
+
+upb_serial = Serial('/dev/ttyMI0', 4800)
+upb = UPB(upb_serial)
+upb.start()
+
+tcp = TCP('192.168.13.146', 9761)
+insteon = InsteonPLM(tcp)
+insteon.start()
+
+sg_serial = Serial('/dev/ttyMI2', 9600)
+sg = Stargate(sg_serial)
+sg.start()
+# Turn on Light - Address 19.05.7b
+response = insteon.on('19.05.7b')
+
+# Turn off Light - Address 19.05.7b
+response2 = insteon.off('19.05.7b')
+
+# Turn on Light - Network: 49, ID: 3
+response = upb.on((49, 3))
+
+# Turn off Light - Network: 49, ID: 3
+response2 = upb.off((49, 3))
+
+def on_digital_input(command=None, address=None):
+    print "Digital Input #" + str(address) + " -> " + command
+
+
+
+
+# Listen for changes on Digital Input #1 on Stargate
+sg.onCommand(callback=on_digital_input, address='D1')
+
+select.select([],[],[])
