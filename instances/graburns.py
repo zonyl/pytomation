260a97ec9bd75fa78c4fa31ba0700d0c731bf485
root <jason@sharpee.com>
2012-08-13 21:52:13 -0400

    Refactor Light device name.  Document some more in the sample instance.

--- instances/graburns.py
+++ instances/graburns.py
@@ -2,6 +2,7 @@ import select
 
 from pytomation.interfaces import UPB, InsteonPLM, TCP, Serial, Stargate
 
+###################### INTERFACE CONFIG #########################
 upb_serial = Serial('/dev/ttyMI0', 4800)
 upb = UPB(upb_serial)
 upb.start()
@@ -13,6 +14,8 @@ insteon.start()
 sg_serial = Serial('/dev/ttyMI2', 9600)
 sg = Stargate(sg_serial)
 sg.start()
+
+###################### DEVICE CONFIG #########################
 # Turn on Light - Address 19.05.7b
 response = insteon.on('19.05.7b')
 
@@ -28,10 +31,8 @@ response2 = upb.off((49, 3))
 def on_digital_input(command=None, address=None):
     print "Digital Input #" + str(address) + " -> " + str(command)
 
-
-
-
 # Listen for changes on Digital Input #1 on Stargate
 sg.onCommand(callback=on_digital_input, address='D1')
 
+# sit and spin - Let the magic flow
 select.select([],[],[])
--- pytomation/devices/__init__.py
+++ pytomation/devices/__init__.py
@@ -1,3 +1,3 @@
 from .ha_device import *
 from .door import *
-from .light_switch import *
+from .light import *
