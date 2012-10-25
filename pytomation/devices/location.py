e872a186750e42085710448b92d5605b5aaa777d
Jason <jason@sharpee.com>
2012-10-24 22:38:28 -0400

    Location Device -> Beginnings of a location device which will tell us if it is Light or Dark in the real world depending on geographical location and date / time.

--- pytomation/devices/location.py
+++ pytomation/devices/location.py
@@ -1,4 +1,30 @@
-from .state import StateDevice
+from .state import StateDevice, State
 
 class Location(StateDevice):
-    pass
+    STATES = [State.LIGHT, State.DARK]
+
+    class MODE():
+        STANDARD = 'standard'
+        CIVIL = 'civil'
+        NAUTICAL = 'nautical'
+        ASTRONOMICAL = 'astronomical'
+        
+    _mode = MODE.STANDARD
+    
+    def __init__(self, latitude, longitude, mode=None):
+        self._latitude = latitude
+        self._longitudue = longitude
+        if mode:
+            self._mode = mode
+
+    @property
+    def mode(self):
+        return self._mode
+    
+    @mode.setter
+    def mode(self, value):
+        self._mode = value
+        self._evaluate()
+
+    def _evaluate(self):
+        pass
--- tests/devices/location.py
+++ tests/devices/location.py
@@ -1,8 +1,17 @@
-from .ha_device import HADevice
+from unittest import TestCase, main
+from mock import Mock, patch
 
-class Location(HADevice):
-    STATES = ['SUNSET', 'TWILIGHT', 'SUNRISE', 'DARK', 'LIGHT']
-    
-    def lat_and_long(self, lat, long):
-        pass
+from pytomation.devices import Location, State
+
+
+class LocationTests(TestCase):
+    def setUp(self):
+        self.loc = Location(35.2269, -80.8433)
+
+    @patch('time')    
+    def test_sunset(self, mockTime):
+        mockTime.now = ''
+        self.loc.mode = Location.MODE.STANDARD
+        self.assertEqual(self.loc.state, State.DARK)
+        
         
