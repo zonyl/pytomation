78f1141259db389f6788c6817e1f8b813f039013
Jason <jason@sharpee.com>
2012-10-25 20:21:08 -0400

    Location -> Finish rough draft of location object.  Should calculate something ...

--- pytomation/devices/location.py
+++ pytomation/devices/location.py
@@ -1,30 +1,69 @@
+import ephem
+
+from datetime import datetime
+from time import strftime
 from .state import StateDevice, State
+from pytomation.utility import CronTimer
 
 class Location(StateDevice):
     STATES = [State.LIGHT, State.DARK]
 
     class MODE():
-        STANDARD = 'standard'
-        CIVIL = 'civil'
-        NAUTICAL = 'nautical'
-        ASTRONOMICAL = 'astronomical'
+        STANDARD = 0
+        CIVIL = -6
+        NAUTICAL = -12
+        ASTRONOMICAL = -18
         
     _mode = MODE.STANDARD
     
-    def __init__(self, latitude, longitude, mode=None):
-        self._latitude = latitude
-        self._longitudue = longitude
+    def __init__(self, latitude, longitude, tz=-5, mode=MODE.STANDARD):
+        self.obs = ephem.Observer()
+        self.obs.lat = latitude
+        self.obs.long = longitude
+        self.tz = tz
+        
+        self.sun = ephem.Sun(self.obs)
         if mode:
-            self._mode = mode
+            self._horizon = mode
+        
+        self._sunset_timer = CronTimer()
+        self._sunrise_timer = CronTimer()
+        self._evaluate()
 
     @property
     def mode(self):
-        return self._mode
+        return self._horizon
     
     @mode.setter
     def mode(self, value):
-        self._mode = value
+        self._horizon = value
         self._evaluate()
 
     def _evaluate(self):
-        pass
+        self.obs.horizon = self._horizon
+        self.obs.date = datetime.now()
+        # What is it now?
+        prev_rising = self.obs.previous_rising(self.sun, use_center=True)
+        prev_setting = self.obs.previous_setting(self.sun, use_center=True)
+        if prev_rising > prev_setting:
+            self.state = State.LIGHT
+        else:
+            self.state = State.DARK
+        # Setup trigger for next transition
+        next_rising = self.obs.next_rising(self.sun, use_center=True)
+        next_setting = self.obs.next_setting(self.sun, use_center=True)
+        
+        self._sunset_time.stop()
+        self._sunset_timer.interval(CronTimer.to_cron(strftime("%H:%M:%S", next_setting)))
+        self._sunset_timer.action(self._evaluate)
+        self._sunset_timer.start()
+
+        self._sunrise_timer.stop()
+        self._sunrise_timer.interval(CronTimer.to_cron(strftime("%H:%M:%S", next_rising)))
+        self._sunrise_timer.action(self._evaluate)
+        self._sunrise_timer.start()
+
+#        self.obs.previous_rising(self.sun, use_center=True)
+#        self.obs.next_setting(self.sun, use_center=True)
+        
+        
