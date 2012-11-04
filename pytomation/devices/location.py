2683932e876f1a223715d5d6fd22c16f84bafc03
Jason <jason@sharpee.com>
2012-11-04 18:31:54 -0500

    Location -> Finish implementation of Location object.  Update unit tests.

--- pytomation/devices/location.py
+++ pytomation/devices/location.py
@@ -1,4 +1,5 @@
 import ephem
+import pytz
 
 from datetime import datetime
 from time import strftime
@@ -16,19 +17,21 @@ class Location(StateDevice):
         
     _mode = MODE.STANDARD
     
-    def __init__(self, latitude, longitude, tz=-5, mode=MODE.STANDARD):
+    def __init__(self, latitude, longitude, tz='US/Eastern', mode=MODE.STANDARD, is_dst=True):
         self.obs = ephem.Observer()
         self.obs.lat = latitude
         self.obs.long = longitude
-        self.tz = tz
-        
+        self.tz = pytz.timezone(tz)
+        self.is_dst = is_dst
+
         self.sun = ephem.Sun(self.obs)
-        if mode:
-            self._horizon = mode
+#        self.sun = ephem.Sun()
+        self._horizon = mode
         
         self._sunset_timer = CronTimer()
         self._sunrise_timer = CronTimer()
-        self._evaluate()
+        self._local_time = None
+        self._recalc()
 
     @property
     def mode(self):
@@ -37,33 +40,76 @@ class Location(StateDevice):
     @mode.setter
     def mode(self, value):
         self._horizon = value
-        self._evaluate()
+        self._recalc()
+        
+        
+    @property
+    def local_time(self):
+        if not self._local_time:
+            return self.tz.localize(datetime.now(), is_dst=self.is_dst)
+        else:
+            return self.tz.localize(self._local_time, is_dst=self.is_dst)
+    
+    @local_time.setter
+    def local_time(self, value):
+        self._local_time = value
+        self._recalc()
 
-    def _evaluate(self):
+    def _recalc(self):
         self.obs.horizon = self._horizon
-        self.obs.date = datetime.now()
-        # What is it now?
-        prev_rising = self.obs.previous_rising(self.sun, use_center=True)
-        prev_setting = self.obs.previous_setting(self.sun, use_center=True)
-        if prev_rising > prev_setting:
+
+#        midnight = self.tz.localize(local_time.replace(hour=12, minute=0, second=0, microsecond=0, tzinfo=None), 
+#                               is_dst=None)
+#        self.obs.date = midnight.astimezone(pytz.utc) 
+
+        self.obs.date = self.local_time.astimezone(pytz.utc)
+
+        prev_rising = self._utc2tz(
+                                  self.obs.previous_rising(self.sun, use_center=True).datetime())
+        prev_setting = self._utc2tz(
+                                  self.obs.previous_setting(self.sun, use_center=True).datetime())
+        self._sunrise = self._utc2tz(
+                                     self.obs.next_rising(self.sun, use_center=True).datetime())
+        self._sunset = self._utc2tz(
+                                     self.obs.next_setting(self.sun, use_center=True).datetime())
+        if self._sunrise > self._sunset:
             self.state = State.LIGHT
         else:
             self.state = State.DARK
+            
         # Setup trigger for next transition
-        next_rising = self.obs.next_rising(self.sun, use_center=True)
-        next_setting = self.obs.next_setting(self.sun, use_center=True)
-        
-        self._sunset_time.stop()
-        self._sunset_timer.interval(CronTimer.to_cron(strftime("%H:%M:%S", next_setting)))
-        self._sunset_timer.action(self._evaluate)
-        self._sunset_timer.start()
+        sunset_t = self._sunset_timer
+#        sunset_t.stop()
+        sunset_t.interval(*CronTimer.to_cron(strftime("%H:%M:%S", self.sunset.timetuple())))
+        sunset_t.action(self._recalc)
+        sunset_t.start()
 
-        self._sunrise_timer.stop()
-        self._sunrise_timer.interval(CronTimer.to_cron(strftime("%H:%M:%S", next_rising)))
-        self._sunrise_timer.action(self._evaluate)
-        self._sunrise_timer.start()
-
-#        self.obs.previous_rising(self.sun, use_center=True)
-#        self.obs.next_setting(self.sun, use_center=True)
+#        self._sunrise_timer.stop()
+        self._sunrise_timer.interval(*CronTimer.to_cron(strftime("%H:%M:%S", self.sunrise.timetuple())))
+        self._sunrise_timer.action(self._recalc, ())
+        self._sunrise_timer.start()    
         
+
+    @property
+    def sunset(self):
+        return self._sunset
+    
+    @sunset.setter
+    def sunset(self, value):
+        self._sunset = value
+        self._recalc()
+        return self._sunset
+    
+    @property
+    def sunrise(self):
+        return self._sunrise
+    
+    @sunrise.setter
+    def sunrise(self, value):
+        self._sunrise = value
+        self._recalc()
+        return self._sunrise
+
+    def _utc2tz(self, value):
+        return pytz.utc.localize(value, is_dst=self.is_dst).astimezone(self.tz)
         
--- tests/devices/location.py
+++ tests/devices/location.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from unittest import TestCase, main
 from mock import Mock, patch
 
@@ -6,12 +8,13 @@ from pytomation.devices import Location, State
 
 class LocationTests(TestCase):
     def setUp(self):
-        self.loc = Location(35.2269, -80.8433)
+        self.loc = Location('35.2269', '-80.8433')
 
-    @patch('time')    
-    def test_sunset(self, mockTime):
-        mockTime.now = ''
-        self.loc.mode = Location.MODE.STANDARD
+    def test_sunset(self):
+        self.loc.local_time = datetime(2012,6,1,0,0,0)
+#        MockDateTime.now = classmethod(lambda x: datetime(2012,6,1,0,0,0))
         self.assertEqual(self.loc.state, State.DARK)
-        
+#        MockDateTime.now = classmethod(lambda x: datetime(2012,6,1,12,0,0))
+        self.loc.local_time = datetime(2012,6,1,12,0,0)
+        self.assertEqual(self.loc.state, State.LIGHT)
         
