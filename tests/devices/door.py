4f980ccdc7d4699938bf0dfd457a8b7b55a65077
Jason <jason@sharpee.com>
2012-11-14 22:22:09 -0500

    Door -> Add a missing test case

--- tests/devices/door.py
+++ tests/devices/door.py
@@ -2,8 +2,7 @@
 from unittest import TestCase, main
 from mock import Mock
 
-from pytomation.devices import Door
-
+from pytomation.devices import Door, State
 
 class DoorTests(TestCase):
     
@@ -14,6 +13,12 @@ class DoorTests(TestCase):
     def test_instantiation(self):
         self.assertIsNotNone(self.device,
                              'Door Device could not be instantiated')
+
+    def test_door_open(self):
+        self.assertEqual(self.device.state, State.UNKNOWN)
+        self.device._on_command('D1', State.ON)
+        self.assertEqual(self.device.state, State.OPEN)
+        
 
 
 if __name__ == '__main__':
