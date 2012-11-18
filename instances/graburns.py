9ed0a689305759dc7987c65b2254ffa7e9c66466
Jason <jason@sharpee.com>
2012-11-17 22:04:38 -0500

    Update Instance Example

--- instances/graburns.py
+++ instances/graburns.py
@@ -1,7 +1,7 @@
 import select
 
 from pytomation.interfaces import UPB, InsteonPLM, TCP, Serial, Stargate
-
+from pytomation.devices import Motion, Door, Light, Location
 ###################### INTERFACE CONFIG #########################
 upb = UPB(Serial('/dev/ttyMI0', 4800))
 upb.start()
@@ -14,24 +14,10 @@ sg.start()
 
 ###################### DEVICE CONFIG #########################
 
+l_foyer = Light((49, 3), (upb))
+l_foyer.on()
+l_foyer.off()
 
-# Turn on Light - Address 19.05.7b
-response = insteon.on('19.05.7b')
-
-# Turn off Light - Address 19.05.7b
-response2 = insteon.off('19.05.7b')
-
-# Turn on Light - Network: 49, ID: 3
-response = upb.on((49, 3))
-
-# Turn off Light - Network: 49, ID: 3
-response2 = upb.off((49, 3))
-
-def on_digital_input(command=None, address=None):
-    print "Digital Input #" + str(address) + " -> " + str(command)
-
-# Listen for changes on Digital Input #1 on Stargate
-sg.onCommand(callback=on_digital_input, address='D1')
 
 # sit and spin - Let the magic flow
 select.select([],[],[])
