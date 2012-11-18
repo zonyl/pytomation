aeff5ad41f306733724373ded4845f9f3cb3209f
root <root@server>
2012-11-17 22:55:07 -0500

    Instance Example -> Simple door open triggers light on.

--- instances/graburns.py
+++ instances/graburns.py
@@ -6,15 +6,16 @@ from pytomation.devices import Motion, Door, Light, Location
 upb = UPB(Serial('/dev/ttyMI0', 4800))
 upb.start()
 
-insteon = InsteonPLM(TCP('192.168.13.146', 9761))
-insteon.start()
+#insteon = InsteonPLM(TCP('192.168.13.146', 9761))
+#insteon.start()
 
 sg = Stargate(Serial('/dev/ttyMI2', 9600))
 sg.start()
 
 ###################### DEVICE CONFIG #########################
 
-l_foyer = Light((49, 3), (upb))
+d_foyer = Door('D1', sg)
+l_foyer = Light((49, 3), (upb, d_foyer))
 l_foyer.on()
 l_foyer.off()
 
