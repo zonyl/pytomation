d4ac1a8a25a8a2932b700955a676985617e866be
Jason <jason@sharpee.com>
2012-12-18 22:45:08 -0500

    State2Device -> Feeling much better about this implementation.

--- pytomation/devices/state2.py
+++ pytomation/devices/state2.py
@@ -1,4 +1,43 @@
 from ..common import PytomationObject
+from ..interfaces import Command
 
-class State2(PytomationObject):
-    pass
+class State2(object):
+    ON = 'on'
+    OFF = 'off'
+    UNKNOWN = 'unknown'
+    
+    
+class State2Device(PytomationObject):
+    STATES = [State2.UNKNOWN, State2.ON, State2.OFF]
+    
+    def __init__(self, *args, **kwargs):
+        super(State2Device, self).__init__(*args, **kwargs)
+        self._state = State2.UNKNOWN
+        self._process_kwargs(kwargs)
+        
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, value):
+        self._state = value
+        return self._state
+    
+    def __getattr__(self, name):
+        if name in (Command.ON, Command.OFF):
+            return lambda *a, **k: self.command(name, *a, **k)
+
+    def command(self, command):
+        if command == Command.ON:
+            self.state = State2.ON
+        elif command == Command.OFF:
+            self.state = State2.OFF
+        
+    
+    def _process_kwargs(self, kwargs):
+        for k, v in kwargs.iteritems():
+            getattr(self, k)(v)
+        
+    def initial(self, state):
+        self.state = state
--- tests/devices/state2.py
+++ tests/devices/state2.py
@@ -1,6 +1,24 @@
 from unittest import TestCase
 
-from pytomation.devices import State2
+from pytomation.devices import State2Device, State2
+from pytomation.interfaces import Command
 
 class State2Tests(TestCase):
-    pass
+    def test_instance(self):
+        self.assertIsNotNone(State2Device())
+
+    def test_unknown_initial(self):
+        self.assertEqual(State2Device().state, State2.UNKNOWN)
+
+    def test_initial(self):
+        device = State2Device(
+                        initial=State2.ON
+                        )
+        self.assertEqual(device.state, State2.ON)
+        
+    def test_command_on(self):
+        device = State2Device()
+        self.assertEqual(device.state, State2.UNKNOWN)
+        device.on()
+        self.assertEqual(device.state, State2.ON)
+        
