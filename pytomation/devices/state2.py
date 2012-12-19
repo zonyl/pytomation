d2016586c0b02bd08931883ffb55a3a6f5018b3d
Jason <jason@sharpee.com>
2012-12-19 07:15:01 -0500

    State2 -> Trying to get more clever.

--- pytomation/devices/state2.py
+++ pytomation/devices/state2.py
@@ -8,7 +8,11 @@ class State2(object):
     
     
 class State2Device(PytomationObject):
-    STATES = [State2.UNKNOWN, State2.ON, State2.OFF]
+    STATES = {
+              State2.UNKNOWN: [],
+              State2.ON: [Command.ON,],
+              State2.OFF: [Command.OFF],
+              }
     
     def __init__(self, *args, **kwargs):
         super(State2Device, self).__init__(*args, **kwargs)
@@ -25,19 +29,40 @@ class State2Device(PytomationObject):
         return self._state
     
     def __getattr__(self, name):
-        if name in (Command.ON, Command.OFF):
+        # Give this object methods for each command supported
+        if self._is_valid_command(name):
             return lambda *a, **k: self.command(name, *a, **k)
 
-    def command(self, command):
+    def command(self, command, *args, **kwargs):
+        state = self._command_state_map(command, *args, **kwargs)
+        if state and self._is_valid_state(state):
+            self.state = state
+
+    def _command_state_map(self, command, *args, **kwargs):
         if command == Command.ON:
-            self.state = State2.ON
+            state = State2.ON
         elif command == Command.OFF:
-            self.state = State2.OFF
-        
-    
+            state = State2.OFF
+        return state
+
     def _process_kwargs(self, kwargs):
+        # Process each initializing attribute as a method call on this object
         for k, v in kwargs.iteritems():
             getattr(self, k)(v)
-        
+            
+    def _is_valid_state(self, state):
+        return state in self.STATES.keys()
+    
+    def _commands_supported(self):
+        supported_commands = []
+        for state, command_group in self.STATES.iteritems():
+            for command in command_group:
+                if command not in supported_commands:
+                    supported_commands.append(command)
+        return supported_commands
+
+    def _is_valid_command(self, command):
+        return command in self._commands_supported()
+
     def initial(self, state):
         self.state = state
