8bee880a3c86b8c18f1d409f75175152889df8e7
Jason <jason@sharpee.com>
2012-12-19 21:19:14 -0500

    State2 -> Add "time" attribute.  Supports multiple times to be specified.

--- tests/devices/state2.py
+++ tests/devices/state2.py
@@ -1,4 +1,7 @@
+import time
+
 from unittest import TestCase
+from datetime import datetime
 
 from pytomation.devices import State2Device, State2
 from pytomation.interfaces import Command
@@ -27,6 +30,39 @@ class State2Tests(TestCase):
         self.assertEqual(device.state, State2.UNKNOWN)
         device.level(80)
         self.assertEqual(device.state, (State2.LEVEL, 80))
-    
-    
+        
+    def test_time_off(self):
+        now = datetime.now()
+        hours, mins, secs = now.timetuple()[3:6]
+        secs = (secs + 2) % 60
+        mins += (secs + 2) / 60
+        trigger_time1 = '{h}:{m}:{s}'.format(
+                                             h=hours,
+                                             m=mins,
+                                             s=secs,
+                                                 )
+        print 'Trigger Time' + trigger_time1
+        secs = (secs + 2) % 60
+        mins += (secs + 2) / 60
+        trigger_time2 = '{h}:{m}:{s}'.format(
+                                             h=hours,
+                                             m=mins,
+                                             s=secs,
+                                                 )
+        print 'Trigger Time' + trigger_time2
+        device = State2Device(
+                              time={
+                                    'command': Command.OFF,
+                                    'time': (trigger_time1, trigger_time2),
+                                    }
+                              )
+        self.assertEqual(device.state, State2.UNKNOWN)
+        time.sleep(3)
+        print datetime.now()
+        self.assertEqual(device.state, State2.OFF)
+        device.on()
+        time.sleep(3)
+        print datetime.now()
+        print device._times
+        self.assertEqual(device.state, State2.OFF)
         
