653b4a8fbcf7bfe7f0403ee9506713cd39da90c5
root <root@server>
2013-01-19 21:13:52 -0500

    More Cleanup.  Filter out junk from pytomation main

--- pytomation.py
+++ pytomation.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
     print 'Pytomation'
     scripts = os.listdir(INSTANCES_DIR)
     for script_name in scripts:
-        print "Found Instance Script: " + script_name
-        stuff = __import__("instances.%s" % script_name)
+	if script_name.lower()[-3:]==".py" and script_name.lower() != "__init__.py":
+	        print "Found Instance Script: " + script_name
+        	stuff = __import__("instances.%s" % script_name)
 #    try:
 #        my_module = __import__("myapp.%s" % script_name, fromlist=["list"])
 #    except ImportError:
