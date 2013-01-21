409d79ba5d2b580ea25ec56d307300b4793394f3
Jason <jason@sharpee.com>
2013-01-20 20:08:13 -0500

    HTTP Server -> Get ready for extending this to provide an API.

--- pytomation/utility/http_server.py
+++ pytomation/utility/http_server.py
@@ -7,15 +7,32 @@ from pytomation.common import config
 file_path = "/tmp"
 
 class PytomationHandlerClass(SimpleHTTPRequestHandler):
+    def __init__(self,req, client_addr, server):
+        self._request = req
+        self._address = client_addr,
+        self._server = server
+        
+        SimpleHTTPRequestHandler.__init__(self, req, client_addr, server)
+    
     def translate_path(self, path):
-	global file_path
-	path = file_path + path
+        global file_path
+        path = file_path + path
         return path
+
+    def do_GET(self):
+        return SimpleHTTPRequestHandler.do_GET(self)
+
+#        response_html = "test"
+#        self.send_response(200)
+#        self.send_header("Content-type", "text/html")
+#        self.send_header("Content-length", len(response_html))
+#        self.end_headers()
+#        self.wfile.write(response_html)
     
 class PytomationHTTPServer(object):
     def __init__(self, address="127.0.0.1", port=8080, path="/tmp", *args, **kwargs):
         global file_path
-	self._address = address
+        self._address = address
         self._port = port
         self._protocol = "HTTP/1.0"
         self._path = path
