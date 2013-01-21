64df2ad2f00cfd6408231c591399162fc1716795
Jason <jason@sharpee.com>
2013-01-21 08:00:43 -0500

    Logging -> Attempt to add logfile rotation.

--- pytomation/common/pyto_logging.py
+++ pytomation/common/pyto_logging.py
@@ -1,13 +1,14 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
 
-from .config import *
+from ..common import config
 
 
 class PytoLogging(object):
     
     def __init__(self, *args, **kwargs):
         self._name = args[0]
-        self._log_file = logging_file
+        self._log_file = config.logging_file
         default_log_file = 'pytomation.log'
         if not self._log_file:
             self._log_file = default_log_file
@@ -17,20 +18,24 @@ class PytoLogging(object):
             self._basic_config(default_log_file)
             
         self._logger = logging.getLogger(self._name)
-        
+        if config.logging_rotate_when:
+            self._logger.addHandler(TimedRotatingFileHandler(filename=config.logging_file,
+                                                             when=config.logging_rotate_when,
+                                                             interval=config.logging_rotate_interval,
+                                                             backupCount=config.logging_rotate_backup ))
         #get module specifics
-        module_level_name = logging_modules.get(self._name, logging_default_level)
+        module_level_name = config.logging_modules.get(self._name, config.logging_default_level)
         if module_level_name:
             module_level = getattr(logging, module_level_name)
             self._logger.setLevel(module_level)
 
 
     def _basic_config(self, filename):
-        log_level = getattr(logging, logging_default_level)
+        log_level = getattr(logging, config.logging_default_level)
         logging.basicConfig(
                             filename=filename,
-                            format=logging_format,
-                            datefmt=logging_datefmt,
+                            format=config.logging_format,
+                            datefmt=config.logging_datefmt,
                             level=log_level,
                             )
         
