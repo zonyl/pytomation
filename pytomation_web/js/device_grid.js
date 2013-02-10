6cd3701b1f35b79055f2c02ffd2bda37fc6d9552
George Farris <farrisg@gmsys.com>
2013-02-10 12:56:43 -0800

    Added drop down selection for web page

--- pytomation_web/js/device_grid.js
+++ pytomation_web/js/device_grid.js
@@ -19,30 +19,58 @@ function reload_device_grid()
 	        return x < y ? -1 : x > y ? 1 : 0;		   		
 		   
 		});
-
+		
+		var select = document.getElementsByName('listDevice')[0];
+	    	    
 		var last_type = null;
 		$.each(data, function(deviceID, values) {
-				rowData = "";
-				if (values['type_name'] != last_type){
-					rowData += "<tr><Td colspan=3>" + values['type_name'] + "<td></tr>";
-					last_type = values['type_name'];
+				if ( select.value == 'all' ) {
+					rowData = "";
+					if (values['type_name'] != last_type){
+						rowData += "<tr><Td colspan=3>" + values['type_name'] + "<td></tr>";
+						last_type = values['type_name'];
+					}
+					rowData += "<tr data-id='" + values['id'] + "'><td></td>";
+					rowData += "<td>" + values['name'] + "</td>";
+					rowData += "<td class='state'>" + values['state'] + "</td>";
+					rowData += "<td class='commands'>";
+					commands = [];
+					if (values['commands']) {
+						$.each(values['commands'], function(index, command){
+							if ('junk' == values['state']) {
+								commands.push("<button class='command btn btn-small' command='" + command + "' deviceId='" + values['id'] + "'>" + command + "</button>");
+							} else {
+								commands.push("<button class='command btn btn-primary btn-small' command='" + command + "' deviceId='" + values['id'] + "'>" + command + "</button>");
+							}
+						});
+					}
+					rowData += commands.join(" ") + "</td></tr>";
+					devices.push(rowData);
+				
+				} else if (values['type_name'] == select.value) {
+					
+					rowData = "";
+					if (values['type_name'] != last_type){
+						rowData += "<tr><Td colspan=3>" + values['type_name'] + "<td></tr>";
+						last_type = values['type_name'];
+					}
+					rowData += "<tr data-id='" + values['id'] + "'><td></td>";
+					rowData += "<td>" + values['name'] + "</td>";
+					rowData += "<td class='state'>" + values['state'] + "</td>";
+					rowData += "<td class='commands'>";
+					commands = [];
+					if (values['commands']) {
+						$.each(values['commands'], function(index, command){
+							if ('junk' == values['state']) {
+								commands.push("<button class='command btn btn-small' command='" + command + "' deviceId='" + values['id'] + "'>" + command + "</button>");
+							} else {
+								commands.push("<button class='command btn btn-primary btn-small' command='" + command + "' deviceId='" + values['id'] + "'>" + command + "</button>");
+							}
+						});
+					}
+					rowData += commands.join(" ") + "</td></tr>";
+					devices.push(rowData);
 				}
-				rowData += "<tr data-id='" + values['id'] + "'><td></td>";
-				rowData += "<td>" + values['name'] + "</td>";
-				rowData += "<td class='state'>" + values['state'] + "</td>";
-				rowData += "<td class='commands'>";
-				commands = [];
-				if (values['commands']) {
-					$.each(values['commands'], function(index, command){
-						if ('junk' == values['state']) {
-							commands.push("<button class='command btn btn-small' command='" + command + "' deviceId='" + values['id'] + "'>" + command + "</button>");
-						} else {
-							commands.push("<button class='command btn btn-primary btn-small' command='" + command + "' deviceId='" + values['id'] + "'>" + command + "</button>");
-						}
-					});
-				}
-				rowData += commands.join(" ") + "</td></tr>";
-				devices.push(rowData);
 			});
 		$("#tableDevices").append(devices.join(''));
 		$(".command").click(on_device_command);
