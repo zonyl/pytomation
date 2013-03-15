5b34f7f81a61e3ad490f9ebcb27963b4498b971c
Jason <jason@sharpee.com>
2013-03-14 21:36:20 -0400

    Mochad -> Take a guess as to what mochad returns when it recieves a command.

--- pytomation/interfaces/mochad.py
+++ pytomation/interfaces/mochad.py
@@ -2,8 +2,15 @@ from .ha_interface import HAInterface
 
 class Mochad(HAInterface):
     
-    def _readInterface(self, lastPacketHash):
-        pass
+#    def _readInterface(self, lastPacketHash):
+#        pass
+
+    def _onCommand(self, command=None, address=None):
+        commands = command.split(' ')
+        if commands[0] == 'pl':
+            address = commands[1]
+            command = commands[2][0:len(commands[2])-1]
+        super(Mochad, self)._onCommand(command=command, address=address)
     
     def __getattr__(self, command):
         return lambda address: self._interface.write('pl ' + address + ' ' + command + "\x0D" ) 
--- tests/interfaces/mochad.py
+++ tests/interfaces/mochad.py
@@ -1,6 +1,9 @@
+import time
+
 from unittest import TestCase
 from mock import Mock
 from pytomation.interfaces import TCP, Mochad
+
 
 class MochadTests(TestCase):
     
@@ -13,4 +16,16 @@ class MochadTests(TestCase):
     def test_on(self):
         self.mochad.on('a1')
         self.tcp.write.assert_called_with('pl a1 on\x0D')
+        
+    def test_receive_off(self):
+        interface = Mock()
+        interface.callback.return_value = True
+        interface.read.return_value = ''
+        m = Mochad(interface)
+        m.onCommand(address='a1', callback=interface.callback)
+        interface.read.return_value = "pl a1 off\x0D"
+        time.sleep(2)
+        interface.read.return_value = ''
+        interface.callback.assert_called_with(address='a1', command='off', source=m)  
+       
         
