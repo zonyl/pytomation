df5b2d5deb679e6d7b5bc26864f8e06ebedc0cf8
Jason Sharpee <jason@sharpee.com>
2013-08-31 07:47:08 -0400

    HW Thermostat -> Finish the rest of the basic commands.  Implement the ability to trigger based on temperature change.

--- pytomation/interfaces/hw_thermostat.py
+++ pytomation/interfaces/hw_thermostat.py
@@ -22,29 +22,60 @@ from .ha_interface import HAInterface
 from .common import *
 
 class HW_Thermostat(HAInterface):
-    
+    def _init(self, *args, **kwargs):
+        super(HW_Thermostat, self)._init(*args, **kwargs)
+        self._last_temp = None
+        
     def _readInterface(self, lastPacketHash):
         #check to see if there is anyting we need to read
         responses = self._interface.read('tstat')
         if len(responses) != 0:
             for response in responses.split():
                 self._logger.debug("[HW Thermostat] Response> " + hex_dump(response))
-                 
+                self._process_current_temp(response)
         else:
             time.sleep(0.1)  # try not to adjust this 
+
+    def heat(self, address):
+        command = ('tstat', json.dumps({'tmode': 1}))
+        commandExecutionDetails = self._sendInterfaceCommand(command)
+#        return self._waitForCommandToFinish(commandExecutionDetails, timeout=2.0)
+
+    def cool(self, address):
+        command = ('tstat', json.dumps({'tmode': 2}))
+        commandExecutionDetails = self._sendInterfaceCommand(command)
+#        return self._waitForCommandToFinish(commandExecutionDetails, timeout=2.0)
+
+    def automatic(self, address):
+        command = ('tstat', json.dumps({'tmode': 3}))
+        commandExecutionDetails = self._sendInterfaceCommand(command)
+#        return self._waitForCommandToFinish(commandExecutionDetails, timeout=2.0)
                  
     def circulate(self, address):
-        command = ('tstat', json.dumps({'fmode': 2}), 'POST')
+        command = ('tstat', json.dumps({'fmode': 2}))
         commandExecutionDetails = self._sendInterfaceCommand(command)
 #        return self._waitForCommandToFinish(commandExecutionDetails, timeout=2.0)
 
     def off(self, address):
-        command = ('tstat', json.dumps({'fmode': 0, 'tmode': 0}), 'POST')
+        command = ('tstat', json.dumps({'fmode': 0, 'tmode': 0}))
         commandExecutionDetails = self._sendInterfaceCommand(command)
 #        return self._waitForCommandToFinish(commandExecutionDetails, timeout=2.0)
     
-    def level(self, address, level, timeout=None, rate=None):
-        pass
+    def level(self, address, level):
+        command = ('tstat', json.dumps({'t_heat': level, 't_cool': level}))
+        commandExecutionDetails = self._sendInterfaceCommand(command)
+#        return self._waitForCommandToFinish(commandExecutionDetails, timeout=2.0)
     
     def version(self):
         self._logger.info("HW Thermostat Pytomation driver version " + self.VERSION + '\n')
+        
+    def _process_current_temp(self, response):
+        temp = None
+        try:
+            status = json.loads(response)
+            temp = status['temp']
+        except Exception, ex:
+            self._logger.error('HW Thermostat couldnt decode status json: ' + str(ex))
+        if temp and temp != self._last_temp:
+            self._onCommand(command=temp)
+        
