e5c2f368241ad8122b0f4a52a4ba1a6cff668e70
Jason Sharpee <jason@sharpee.com>
2013-09-01 09:02:55 -0400

    XMPP -> Add tons of debugging info.

--- pytomation/devices/xmpp.py
+++ pytomation/devices/xmpp.py
@@ -11,6 +11,7 @@ class XMPP(StateDevice):
     COMMANDS = [Command.MESSAGE]
 
     def __init__(self, *args, **kwargs):
+        self._xmpp = None
         super(XMPP, self).__init__(*args, **kwargs)
 
     def _initial_vars(self, *args, **kwargs):
@@ -20,46 +21,57 @@ class XMPP(StateDevice):
         self._server = kwargs.get('server', None)
         self._port = kwargs.get('port', None)
 
-        self._xmpp = ClientXMPP(self._id, self._password)
-
-        self._xmpp.add_event_handler("session_start", self._session_start)
-        self._xmpp.add_event_handler("message", self._message)
-        self._xmpp.add_event_handler("ssl_invalid_cert", self._invalid_cert)
-
-        self._logger.info('Connecting to server for id:{id} ({server})'.format(
-                                               id=self._xmpp._id,
-                                               server=self._server,
-                                                                               ))
-        status = None
-        if self._server or self._port:
-            status = self._xmpp.connect((self._server, self._port))
+        if not self._xmpp:
+            self._xmpp = ClientXMPP(self._id, self._password)
+    
+            self._xmpp.add_event_handler("session_start", self._session_start)
+            self._xmpp.add_event_handler("message", self._message)
+            self._xmpp.add_event_handler("ssl_invalid_cert", self._invalid_cert)
+    
+            self._logger.info('Connecting to server for id:{id} ({server})'.format(
+                                                   id=self._xmpp._id,
+                                                   server=self._server,
+                                                                                   ))
+            status = None
+            if self._server or self._port:
+                status = self._xmpp.connect((self._server, self._port))
+            else:
+                status = self._xmpp.connect()
+            self._logger.info("Connection Result: {0}".format( status))
+    
+            self._xmpp.process(block=False)
+            self._logger.debug('Processing')
         else:
-            status = self._xmpp.connect()
-        self._logger.info("Connection Result: {0}".format( status))
-
-        self._xmpp.process(block=False)
-        self._logger.debug('Processing')
+            self._logger.debug('Here twice?')
         
     def _session_start(self, event):
+        self._logger.debug('Session Start')
         self._xmpp.send_presence()
         self._xmpp.get_roster()
+        self._logger.debug('Session End')
         
     def _message(self, msg):
+        self._logger.debug('REceived Message')
         if msg['type'] in ('chat', 'normal'):
             msg.reply("Thanks for sending\n%(body)s" % msg).send()
 
 
     def _invalid_cert(self, pem_cert):
+        self._logger.debug('Cert')
         der_cert = ssl.PEM_cert_to_DER_cert(pem_cert)
         try:
             cert.verify('talk.google.com', der_cert)
-            logging.debug("CERT: Found GTalk certificate")
+            self._logger.debug("CERT: Found GTalk certificate")
         except cert.CertificateError as err:
-            log.error(err.message)
+            self._logger.error(err.message)
             self.disconnect(send_close=False)
+        self._logger.debug('Cert End')
 
     def _delegate_command(self, command, *args, **kwargs):
+        self._logger.debug('Delegating')
+
         if isinstance(command, tuple) and command[0] == Command.MESSAGE:            
+            self._logger.debug('Sending Message')
             result = self._xmpp.send_message(mto=command[1],
                                       mbody=command[2],
                                       mtype='chat')
