189469bae4c79a966fc534d15e08dc5139c58378
Jason Sharpee <jason@sharpee.com>
2013-09-23 06:35:15 -0400

    Mock Interface -> Allow the ability to query the written data

--- tests/common/mock_interface.py
+++ tests/common/mock_interface.py
@@ -5,25 +5,32 @@ from pytomation.interfaces.common import *
 class Mock_Interface(object):
     def __init__(self, *args, **kwargs):
         super(Mock_Interface, self).__init__(*args, **kwargs)
-        self.read_data = ""
+        self._read_data = ""
+        self._write_data = []
         
     def read(self, count=None):
         print 'Reading for {0} bytes'.format(count)
         if count:
-            data = self.read_data[:count]
-            self.read_data = self.read_data[count:]
+            data = self._read_data[:count]
+            self._read_data = self._read_data[count:]
         else:
-            data = self.read_data
-            self.read_data = ""
+            data = self._read_data
+            self._read_data = ""
             
         print 'Returning data hhhh:' + hex_dump(data) + ":"
         return data
 
-    def write(self, data=None):
+    def write(self, data=None, **kwargs):
+        #print 'kkkkk' + str(kwargs)
+        self._write_data.append(data)
         return True
     
     def put_read_data(self, data):
         print 'Adding data: ' + hex_dump(data) + ":"
-        self.read_data += data
+        self._read_data += data
     
-    
+    def query_write_data(self):
+        return self._write_data
+
+    def clear_write_data(self):
+        self._write_data = []
