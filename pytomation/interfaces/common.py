345ef70ec9d0b05c09eb652817cc6eb01e7c2b64
Jason Sharpee <jason@sharpee.com>
2013-11-07 22:49:16 -0500

    Common -> Handle issues with hex dump

--- pytomation/interfaces/common.py
+++ pytomation/interfaces/common.py
@@ -444,12 +444,16 @@ FILTER=''.join([(len(repr(chr(x)))==3) and chr(x) or '.' for x in range(256)])
 
 def hex_dump(src, length=8):
     N=0; result=''
-    while src:
-        s,src = src[:length],src[length:]
-        hexa = ' '.join(["%02X"%ord(x) for x in s])
-        s = s.translate(FILTER)
-        result += "%04X   %-*s   %s\n" % (N, length*3, hexa, s)
-        N+=length
+    try:
+        while src:
+            s,src = src[:length],src[length:]
+            hexa = ' '.join(["%02X"%ord(x) for x in s])
+            s = s.translate(FILTER)
+            result += "%04X   %-*s   %s\n" % (N, length*3, hexa, s)
+            N+=length
+    except Exception, ex:
+        print 'Exception in Hexdump: ' + str(ex)
+        result = src
     return result
 
 ## end of http://code.activestate.com/recipes/142812/ }}}
