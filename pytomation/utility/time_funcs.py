21986f6719c99eeed39fa676cc2a24bc0336dc48
zonyl <jason@sharpee.com>
2013-11-30 20:22:39 -0500

    CronTimeRange -> Forgot to clean up after myself.

--- pytomation/utility/time_funcs.py
+++ pytomation/utility/time_funcs.py
@@ -15,16 +15,3 @@ def crontime_in_range(item, start, end):
         return True
 
     return False
-    
-#     s_s, s_m, s_h = start[0:3]
-#     e_s, e_m, e_h = end[0:3]
-#     i_s, i_m, i_h = item[0:3]
-#     if e_h >= s_h and e_m >= s_m and e_s >= s_s:
-#         if i_h >= s_h and i_m >= s_m and i_s >= s_s and \
-#             i_h <= e_h and i_m <= e_m and i_s <= e_s:
-#                 return True
-#     else:
-#         if i_h >= s_h and i_m >= s_m and i_s >= s_s or \
-#             i_h <= e_h and i_m <= e_m and i_s <= e_s:
-#                 return True
-#     return False
