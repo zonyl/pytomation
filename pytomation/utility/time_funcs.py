7ae10be8c42f002a1151ee814a007d63f39fd975
zonyl <jason@sharpee.com>
2013-12-01 08:24:34 -0500

    CronTimeRange -> Fix namespace collision with DateTime

--- pytomation/utility/time_funcs.py
+++ pytomation/utility/time_funcs.py
@@ -1,14 +1,14 @@
-import datetime
+from datetime import datetime, timedelta
 
 def crontime_in_range(item, start, end):
-    startDt = datetime.datetime.strptime("{2}:{1}:{0}".format(*start[0:3]), '%H:%M:%S')
-    endDt = datetime.datetime.strptime("{2}:{1}:{0}".format(*end[0:3]), '%H:%M:%S')
-    itemDt = datetime.datetime.strptime("{2}:{1}:{0}".format(*item[0:3]), '%H:%M:%S')
+    startDt = datetime.strptime("{2}:{1}:{0}".format(*start[0:3]), '%H:%M:%S')
+    endDt = datetime.strptime("{2}:{1}:{0}".format(*end[0:3]), '%H:%M:%S')
+    itemDt = datetime.strptime("{2}:{1}:{0}".format(*item[0:3]), '%H:%M:%S')
 
     if startDt > endDt:
-        endDt=endDt + datetime.timedelta(days=1)
+        endDt=endDt + timedelta(days=1)
         if itemDt < startDt:
-            itemDt = itemDt + datetime.timedelta(days=1)
+            itemDt = itemDt + timedelta(days=1)
 
     if startDt <= itemDt <= endDt:
         #should technically check dates in cron as well.. for another day
