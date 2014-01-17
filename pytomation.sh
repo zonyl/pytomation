18eacc64020923bff43317deaec1bf9fecfb2576
George Clark <geonwiki@fenachrone.com>
2014-01-17 17:53:08 -0500

    Add logging for STDERR so faults are not missed
    And add some tags into the logfiles so that the restart can be found.

--- pytomation.sh
+++ pytomation.sh
@@ -1,9 +1,18 @@
-#!/bin/bash 
+#!/bin/bash
+
+# Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
+exec >> /var/log/pyto.log
+exec 2>&1
 
 # It's possible PYHOME in /usr/bin has been changed by the install.sh script
 PYHOME="/home/pytomation"
 cd $PYHOME
 PROGRAM='python ./pytomation.py'
+
+# Put a flag into the logs so we can find when pytomation was restarted
+echo "=========RESTART==========" >> ./pylog.log
+echo "=========RESTART=========="
+
 $PROGRAM &
 PID=$!
 if [ -w /var/run ]; then
@@ -11,5 +20,4 @@ if [ -w /var/run ]; then
 else
     echo "Running as regular user can't write PID file to /var/run/pytomation.pid..."
     echo "PID is $PID..."
-	echo $PID > pytomation.pid
 fi
