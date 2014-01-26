f87c909558d8518486c0a6b58079191a12c893e8
Jason Sharpee <jason@sharpee.com>
2014-01-26 14:24:20 -0500

    WeMo -> Fix regression tests.

--- tests/interfaces/wemo.py
+++ tests/interfaces/wemo.py
@@ -1,14 +1,15 @@
 import time
 
 from unittest import TestCase
+from tests.common import MockInterface
 
 from pytomation.interfaces import WeMo
 
 class WeMoTests(TestCase):
     def setUp(self):
 #        self.interface = WeMo( '192.168.13.141', '49153')
-        self.interface = WeMo( '192.168.13.141', '49153')
-
+        self.interface = WeMo(MockInterface())
+        
     def test_instantiation(self):
         self.assertIsInstance(self.interface, WeMo)
     
