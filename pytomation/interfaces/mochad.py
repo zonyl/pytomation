d1c7cff556a1383595b9b669479054c33f3ea184
texnofobix <texnofobix@gmail.com>
2014-01-27 23:21:22 -0500

    Fixing issues with lamba and web interface by specifying on and off.

--- pytomation/interfaces/mochad.py
+++ pytomation/interfaces/mochad.py
@@ -1,24 +1,58 @@
 from .ha_interface import HAInterface
-import time
+#from pytomation.devices import State
 
 class Mochad(HAInterface):
     
+    VERSION='0.2'
+
 #    def _readInterface(self, lastPacketHash):
-#        pass
+#        pass     
 
-    def _readInterface(self, lastPacketHash):
-       time.sleep(0.5)
+    def rawstatus(self):
+        self._interface.write('st'+"\x0D")
+        return self._interface.read()
 
-    def status(self):
-       self._interface.write('st'+"\x0D")
-       return self._interface.read()
+    def status(self,address):
+        """
+        print "Status of Address: " + str(address)
+        self._interface.write('st'+"\x0D")
+        data=self._interface.read()
+        
+        if len(address)==2:   #normal housecode address
+	   print address[0].upper()
+	   return State.ON
+
+        if len(address)==6:   #sensor address
+           return State.ON
+	    """
+        self._logger.debug('Mochad Status called')
+        return  
+    
+    def update_status(self):
+        self._logger.debug('Mochad update status called')
+        for d in self._devices:
+            self._logger.debug('... '+ d.address)
+            self.status(d.address)
 
     def _onCommand(self, command=None, address=None):
         commands = command.split(' ')
-        if commands[0] == 'pl':
+        if commands[0] == 'rf':
             address = commands[1]
             command = commands[2][0:len(commands[2])-1]
+        self._logger.debug('[Mochad] Command>'+command+' at '+address)
         super(Mochad, self)._onCommand(command=command, address=address)
     
+    """ 
     def __getattr__(self, command):
         return lambda address: self._interface.write('rf ' + address + ' ' + command + "\x0D" ) 
+    """
+
+    def on(self, address):
+        self._interface.write('rf ' + address + ' on' + "\x0D")
+
+    def off(self, address):
+        self._interface.write('rf ' + address + ' off'+ "\x0D")
+
+    def version(self):
+        self._logger.info("Mochad Pytomation Driver version " + self.VERSION)
+
