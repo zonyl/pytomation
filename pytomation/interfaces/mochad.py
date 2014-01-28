bb431177fe6e75fb6f8c4960e382087d624df07e
texnofobix <texnofobix@gmail.com>
2014-01-28 12:36:54 -0500

    Improving mochad driver with motion sensors and states

--- pytomation/interfaces/mochad.py
+++ pytomation/interfaces/mochad.py
@@ -1,3 +1,4 @@
+from .common import *
 from .ha_interface import HAInterface
 #from pytomation.devices import State
 
@@ -5,8 +6,39 @@ class Mochad(HAInterface):
     
     VERSION='0.2'
 
-#    def _readInterface(self, lastPacketHash):
-#        pass     
+    def _readInterface(self, lastPacketHash):
+        """
+	01/27 23:41:23 Rx RF HouseUnit: A3 Func: Off
+	0     1        2  3  4          5  6     7 
+	01/27 23:48:23 Rx RF HouseUnit: A1 Func: On
+	12/07 20:49:37 Rx RFSEC Addr: C6:1B:00 Func: Motion_alert_MS10A
+	0     1        2  3     4     5        6     7
+	"""
+        responses = self._interface.read()
+	if len(responses) >=4:
+	    self._logger.debug('responses> ' + responses)
+	    data=responses.split(' ')
+	    #ate=data[0]
+	    #ime=data[1]
+	    direction=data[2]
+	    method=data[3]
+	    #ua=data[4]
+	    addr=data[5]
+	    #func junk
+	    func=data[7].strip()
+	    #self._onCommand(command=func.upper(),address=address)
+
+	    if func=="On":
+	        print "on"
+	        self.on(address=addr)
+	    elif func=="Off":
+	        self.off(address=addr)
+	    elif func=="Motion_alert_MS10A":
+	        self._onCommand(command=Command.MOTION,address=addr)
+	    elif func=="Motion_normal_MS10A":
+	        self._onCommand(command=Command.STILL,address=addr)
+
+	    
 
     def rawstatus(self):
         self._interface.write('st'+"\x0D")
@@ -48,9 +80,13 @@ class Mochad(HAInterface):
     """
 
     def on(self, address):
+        self._logger.debug('[Mochad] Command on at '+address)
+	self._onCommand(command=Command.ON,address=address)
         self._interface.write('rf ' + address + ' on' + "\x0D")
 
     def off(self, address):
+        self._logger.debug('[Mochad] Command off at '+address)
+	self._onCommand(command=Command.OFF,address=address)
         self._interface.write('rf ' + address + ' off'+ "\x0D")
 
     def version(self):
