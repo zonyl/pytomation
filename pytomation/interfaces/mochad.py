dd9bce086885ed9a510ef557f0fa4533b745381c
texnofobix <texnofobix@gmail.com>
2014-02-18 22:49:02 -0500

    Mochad -> simplified read. corrected out of index errors. Next steps, is to add in State setting for security devices.

--- pytomation/interfaces/mochad.py
+++ pytomation/interfaces/mochad.py
@@ -3,110 +3,109 @@ from .ha_interface import HAInterface
 #from pytomation.devices import State
 '''
 http://sourceforge.net/apps/mediawiki/mochad/index.php?title=Mochad_Reference
+
+switched pl method for rf as both the cm19a only supports rf while the cm14a supports both pl and rf
 '''
 
 class Mochad(HAInterface):
     
-    VERSION='0.2.2'
+    VERSION='0.3.0'
         
-#     def _init(self, *args, **kwargs):
-#         super(Mochad, self)._init(*args, **kwargs)
+    def _init(self, *args, **kwargs):
+        self._devicestatus = False
+        self._securitystatus = False
+        super(Mochad, self)._init(*args, **kwargs)
 
     def _readInterface(self, lastPacketHash):
-        """
-    	01/27 23:41:23 Rx RF HouseUnit: A3 Func: Off
-    	0     1        2  3  4          5  6     7 
-    	01/27 23:48:23 Rx RF HouseUnit: A1 Func: On
-    	12/07 20:49:37 Rx RFSEC Addr: C6:1B:00 Func: Motion_alert_MS10A
-    	0     1        2  3     4     5        6     7
-	    """
+        raw_response = self._interface.read()
         
-        responses = self._interface.read()
-        if len(responses) == 0:
+        if len(raw_response) == 0: #if no data leave
             return
         
-        response = responses.split('\n')
-        if len(response) < 3 :
-            self._logger.debug('responses> ' + responses)
-            data=responses.split(' ')
-            #date=data[0]
-            #time=data[1]
-            direction=data[2]
-            method=data[3]
-            ua=data[4]
-            addr=data[5]
-            #func junk
-            
-            #if direction=="Rx":
-            func=data[7].strip().rsplit('_',1)[0] #removing _devicemodel
-            print func,addr
-            self._map(func,addr)
+        #NOTE need to correct: Look for number of words first then 3rd term. Then decide what to do
+        
+        response_lines = raw_response.split('\n')
+        
+        self._logger.debug('Number of Lines '+str(len(response_lines)))
+        
+        for line in response_lines:
+            if line.strip()=='':
+                return #leave if empty
+            self._logger.debug('line responses> ' + line)
+            line_data=line.split(' ')
+            if line_data[2]=='Rx' or line_data[2]=='Tx':
+                """ Like below
+                01/27 23:41:23 Rx RF HouseUnit: A3 Func: Off
+                0     1        2  3  4          5  6     7 
+                01/27 23:48:23 Rx RF HouseUnit: A1 Func: On
+                12/07 20:49:37 Rx RFSEC Addr: C6:1B:00 Func: Motion_alert_MS10A
+                0     1        2  3     4     5        6     7
+                """
+                #date=data[0]
+                #time=data[1]
+                #direction=data[2]
+                #method=data[3]
+                #ua=data[4]
+                addr=line_data[5]
+                func=data[7].strip().rsplit('_',1)[0]   #removing _devicemodel
+                self._map(func,addr)
                 
+            """
+            command sent > st
+            02/01 16:44:23 Device selected
+            02/01 16:44:23 House A: 2
+            02/01 16:44:23 House B: 1
+            02/01 16:44:23 Device status
+            02/01 16:44:23 House A: 1=0,2=0,3=0,4=1,5=1,6=1,7=1,8=1,10=0,11=0
+            0     1        2     3  4
+            02/01 16:44:23 Security sensor status
+            02/01 16:44:23 Sensor addr: 000003 Last: 1102:40 Arm_KR10A
+            02/01 16:44:23 Sensor addr: 000093 Last: 1066:33 Disarm_SH624
+            02/01 16:44:23 Sensor addr: 055780 Last: 1049:59 Contact_alert_max_DS10A
+            02/01 16:44:23 Sensor addr: 27B380 Last: 01:42 Motion_normal_MS10A
+            02/01 16:44:23 Sensor addr: AF1E00 Last: 238:19 Lights_Off_KR10A
+            0     1        2      3     4      5     6      7 
+            02/01 16:44:23 End status
+            """    
+                
+            if line_data[2]=='Device':
+                if line_data[3]=='status':
+                    self._devicestatus = True
+                    continue
+
+            if line_data[2]=='Security':
+                if line_data[3]=='sensor':
+                    self._devicestatus = False
+                    self._securitystatus = True
+                    continue
                     
-        """
-        command sent > st
-        02/01 16:44:23 Device selected
-        02/01 16:44:23 House A: 2
-        02/01 16:44:23 House B: 1
-        02/01 16:44:23 Device status
-        02/01 16:44:23 House A: 1=0,2=0,3=0,4=1,5=1,6=1,7=1,8=1,10=0,11=0
-        0     1        2     3  4
-        02/01 16:44:23 Security sensor status
-        02/01 16:44:23 Sensor addr: 000003 Last: 1102:40 Arm_KR10A
-        02/01 16:44:23 Sensor addr: 000093 Last: 1066:33 Disarm_SH624
-        02/01 16:44:23 Sensor addr: 055780 Last: 1049:59 Contact_alert_max_DS10A
-        02/01 16:44:23 Sensor addr: 27B380 Last: 01:42 Motion_normal_MS10A
-        02/01 16:44:23 Sensor addr: AF1E00 Last: 238:19 Lights_Off_KR10A
-        0     1        2      3     4      5     6      7 
-        02/01 16:44:23 End status
-        """    
-        if len(response) > 3:
-            #print len(response)
-            print "Status output?"
-            _devicestatus = False
-            _securitystatus = False
-            #print response
-            for line in response:
-                words = line.split(' ')   
-                print words                
+            if line_data[2]=='End':
+                self._devicestatus = False #shouldn't need to set this false, but why not?
+                self._securitystatus = False
+                continue
+                
+            if self._devicestatus:      
+                housecode = line_data[3].strip(":")
                     
-                if _devicestatus:                   
-                    housecode = words[3].strip(":")
+                for device in line_data[4].split(','):
+                    qdevicestatus=device.split('=')
+                   
+                    if qdevicestatus[1]=='0':
+                          self._onCommand(command=Command.OFF,address=str(housecode+qdevicestatus[0]))
+                    if qdevicestatus[1]=='1':
+                          self._onCommand(command=Command.ON,address=str(housecode+qdevicestatus[0]))
                        
-                    for device in words[4].split(','):
-                        print device
-                        devicestatus=device.split('=')
-                        print devicestatus
-                        print housecode+devicestatus[0]+" is "+devicestatus[1]
-                      
-                        if devicestatus[1]=='0':
-                              self._onCommand(command=Command.OFF,address=str(housecode+devicestatus[0]))
-                        if devicestatus[1]=='1':
-                              self._onCommand(command=Command.ON,address=str(housecode+devicestatus[0]))
-                      
-#                 if _securitystatus:
-#                     print "Function: "+ func + " Address " + addr[0:2]+":"+addr[2:4]+":"+addr[4:6]
-#                     addr=words[4]
-#                     func = words[7].rsplit('_',1)[0]
-#                       
-#                     self._map(func,addr[0:2]+":"+addr[2:4]+":"+addr[4:6]) #adding back :s
-                 
-                if words[2:4] == ["Device","status"]:
-                    print "Begin status"        
-                    _devicestatus=True
-                      
-                if words[2:4] == ["Security","sensor"]:
-                    print "Beginning of security sensor stuff"
-                    _devicestatus=False
-                    _securitystatus=True
- 
-                if words[2:4] == ["End","status"]:
-                    print "End status"
-                    _securitystatus=False
-                    return
+            if self._securitystatus:
+                addr=line_data[4]
+                func = line_data[7].rsplit('_',1)[0]
+                self._logger.debug("Function: "+ func + " Address " + addr[0:2]+":"+addr[2:4]+":"+addr[4:6])
+                # NOTE THIS NEEDS TO SET Security state!       
+
+                  
+
     
     def status(self,address):
-        self._logger.debug('[Mochad] Querying of last known status all devices including '+address)
+        self._logger.debug('Querying of last known status all devices including '+address)
         self._interface.write('st'+"\x0D")
         return None 
         
@@ -119,34 +118,34 @@ class Mochad(HAInterface):
         if commands[0] == 'rf':
             address = commands[1]
             command = commands[2][0:len(commands[2])-1]
-        self._logger.debug('[Mochad] Command>'+command+' at '+address)
+        self._logger.debug('Command>'+command+' at '+address)
         super(Mochad, self)._onCommand(command=command, address=address)
     
-    """ #Causes issues with web interface 
+    """ #Causes issues with web interface Disabling this feature.
     def __getattr__(self, command):
         return lambda address: self._interface.write('rf ' + address + ' ' + command + "\x0D" ) 
     """
 
     def on(self, address):
-        self._logger.debug('[Mochad] Command on at '+address)
+        self._logger.debug('Command on at '+address)
         self._interface.write('rf ' + address + ' on' + "\x0D")
 
     def off(self, address):
-        self._logger.debug('[Mochad] Command off at '+address)
+        self._logger.debug('Command off at '+address)
         self._interface.write('rf ' + address + ' off'+ "\x0D")
         
     def disarm(self, address):
-        self._logger.debug('[Mochad] Command disarm at '+address)
+        self._logger.debug('Command disarm at '+address)
         self._interface.write('rfsec ' + address + ' disarm'+ "\x0D")
         
     def arm(self, address):
-        self._logger.debug('[Mochad] Command sarm at '+address)
+        self._logger.debug('Command arm at '+address)
         self._interface.write('rfsec ' + address + ' arm'+ "\x0D")
 
     def version(self):
         self._logger.info("Mochad Pytomation Driver version " + self.VERSION)
         
-    def _map(self,func,addr):
+    def _map(self,func,addr): #mapping output to a valid command
         #print func,addr
         if func=="On":
             self._onCommand(command=Command.ON,address=addr)
@@ -165,11 +164,11 @@ class Mochad(HAInterface):
         elif func=="Motion_normal":
             self._onCommand(command=Command.STILL,address=addr)
         elif func=="Arm":
-            self._onCommand(command=Command.VACATE,address=addr)
+            self._onCommand(command=Command.ON,address=addr)
         elif func=="Panic":
             self._onCommand(command=Command.VACATE,address=addr)
         elif func=="Disarm":
-            self._onCommand(command=Command.OCCUPY,address=addr)
+            self._onCommand(command=Command.DISARM,address=addr)
         elif func=="Lights_On":
             self._onCommand(command=Command.ON,address=addr)
         elif func=="Lights_Off":
