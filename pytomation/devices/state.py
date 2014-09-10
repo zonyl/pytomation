db64d622752b2eca9093841543dcf3b93f9d703e
Jason Sharpee <jason@sharpee.com>
2014-09-09 22:17:38 -0400

    State Device -> Add onStatechanged method to call custom functions.   Method takes a state parameter and kwargs for prev and source.

--- pytomation/devices/state.py
+++ pytomation/devices/state.py
@@ -69,6 +69,7 @@ class StateDevice(PytomationObject):
         self._last_set = datetime.now()
         self._changes_only = False
         self._delegates = []
+        self._delegates_state_change = []
         self._times = []
         self._maps = {}
         self._delays = {}
@@ -115,6 +116,7 @@ class StateDevice(PytomationObject):
         source = kwargs.get('source', None)
         if value != self._state:
             self._previous_state = self._state
+            self._delegate_state_change(value, prev=self._state, source=source)
         self._last_set = datetime.now()
         self._state = value
         return self._state
@@ -457,6 +459,16 @@ class StateDevice(PytomationObject):
                                                                                    delegate=delegate.name,
                                                                            ))
                 
+    def _delegate_state_change(self, state, *args, **kwargs):
+        for _delegate in self._delegates_state_change:
+            try:
+                _delegate(state, 
+                          prev=kwargs.get('prev', None),
+                          source=kwargs.get('source',None))
+            except Exception, ex:
+                pass
+            
+        
     def devices(self, *args, **kwargs):
         devices = args[0]
 
@@ -855,7 +867,11 @@ class StateDevice(PytomationObject):
         elif new_state != original_state and self._retrigger_delay:
             self._retrigger_delay.restart()
         return False          
-
+    
+    def onStateChanged(self, func):
+        self._delegates_state_change.append(func)
+        return True
+    
     @staticmethod
     def dump_garbage():
         """
