301a2f265bb8f14c3b1ff92eb33c248c861f70f9
David Heaps <king.dopey.10111@gmail.com>
2014-11-29 13:22:43 -0800

    Added HomeControl project, for mobile devices   Contains barebones Apache Cordova project files and plugins used   You must add the platform you wish to build for and have those build tools installed

---
+++ HomeControl/plugins/com.phonegap.plugins.speech/LanguageDetailsChecker.java
@@ -0,0 +1,59 @@
+/**
+ * The MIT License
+ *
+ *	Copyright (c) 2011-2013
+ *	Colin Turner (github.com/koolspin)  
+ *	Guillaume Charhon (github.com/poiuytrez)  
+ *	
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *	
+ *	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *	
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *
+ */
+
+package com.phonegap.plugins.speech;
+
+import java.util.List;
+
+import org.apache.cordova.CallbackContext;
+import org.json.JSONArray;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+
+public class LanguageDetailsChecker extends BroadcastReceiver
+{
+    private List<String> supportedLanguages;
+    private CallbackContext callbackContext;
+
+    public LanguageDetailsChecker(CallbackContext callbackContext) {
+    	super();
+    	this.callbackContext = callbackContext;
+    }
+    
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Bundle results = getResultExtras(true);
+        
+		// get the list of supported languages
+        if (results.containsKey(RecognizerIntent.EXTRA_SUPPORTED_LANGUAGES))
+        {            
+            // Convert the map to json
+            supportedLanguages = results.getStringArrayList(RecognizerIntent.EXTRA_SUPPORTED_LANGUAGES);
+            JSONArray jsonLanguages = new JSONArray(supportedLanguages);
+            callbackContext.success(jsonLanguages);
+        } else {
+        	callbackContext.error("Could not retrieve the list of supported languages");
+        }
+	}
+	
+	public List<String> getSupportedLanguages(){
+		return supportedLanguages;
+	}
+}
---
+++ HomeControl/plugins/com.phonegap.plugins.speech/SpeechRecognizer.java
@@ -0,0 +1,139 @@
+/**
+ * The MIT License
+ *
+ *	Copyright (c) 2011-2013
+ *	Colin Turner (github.com/koolspin)  
+ *	Guillaume Charhon (github.com/poiuytrez)  
+ *	
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *	
+ *	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *	
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *
+ */
+package com.phonegap.plugins.speech;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import org.json.JSONArray;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import android.util.Log;
+import android.app.Activity;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+
+/**
+ * Style and such borrowed from the TTS and PhoneListener plugins
+ */
+public class SpeechRecognizer extends CordovaPlugin {
+    private static final String LOG_TAG = SpeechRecognizer.class.getSimpleName();
+    private static int REQUEST_CODE = 1001;
+
+    private CallbackContext callbackContext;
+    private LanguageDetailsChecker languageDetailsChecker;
+
+    //@Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+		Boolean isValidAction = true;
+
+    	this.callbackContext= callbackContext;
+
+		// Action selector
+    	if ("startRecognize".equals(action)) {
+            // recognize speech
+            startSpeechRecognitionActivity(args);     
+        } else if ("getSupportedLanguages".equals(action)) {
+        	getSupportedLanguages();
+        } else {
+            // Invalid action
+        	this.callbackContext.error("Unknown action: " + action);
+        	isValidAction = false;
+        }
+    	
+        return isValidAction;
+
+    }
+
+    // Get the list of supported languages
+    private void getSupportedLanguages() {
+    	if (languageDetailsChecker == null){
+    		languageDetailsChecker = new LanguageDetailsChecker(callbackContext);
+    	}
+    	// Create and launch get languages intent
+    	Intent detailsIntent = new Intent(RecognizerIntent.ACTION_GET_LANGUAGE_DETAILS);
+    	cordova.getActivity().sendOrderedBroadcast(detailsIntent, null, languageDetailsChecker, null, Activity.RESULT_OK, null, null);
+		
+	}
+
+	/**
+     * Fire an intent to start the speech recognition activity.
+     *
+     * @param args Argument array with the following string args: [req code][number of matches][prompt string]
+     */
+    private void startSpeechRecognitionActivity(JSONArray args) {
+        int maxMatches = 0;
+        String prompt = "";
+        String language = Locale.getDefault().toString();
+
+        try {
+            if (args.length() > 0) {
+            	// Maximum number of matches, 0 means the recognizer decides
+                String temp = args.getString(0);
+                maxMatches = Integer.parseInt(temp);
+            }
+            if (args.length() > 1) {
+            	// Optional text prompt
+                prompt = args.getString(1);
+            }
+            if (args.length() > 2) {
+            	// Optional language specified
+            	language = args.getString(2);
+            }
+        }
+        catch (Exception e) {
+            Log.e(LOG_TAG, String.format("startSpeechRecognitionActivity exception: %s", e.toString()));
+        }
+
+        // Create the intent and set parameters
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, language);
+
+        if (maxMatches > 0)
+            intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, maxMatches);
+        if (!prompt.equals(""))
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, prompt);
+        cordova.startActivityForResult(this, intent, REQUEST_CODE);
+    }
+
+    /**
+     * Handle the results from the recognition activity.
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            // Fill the list view with the strings the recognizer thought it could have heard
+            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+            returnSpeechResults(matches);
+        }
+        else {
+            // Failure - Let the caller know
+            this.callbackContext.error(Integer.toString(resultCode));
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void returnSpeechResults(ArrayList<String> matches) {
+        JSONArray jsonMatches = new JSONArray(matches);
+        this.callbackContext.success(jsonMatches);
+    }
+    
+}
