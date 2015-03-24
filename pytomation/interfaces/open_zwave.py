ee6c717bbe198e09034a685c2bd7fd8a2fb43ab2
texnofobix <texnofobix@gmail.com>
2015-03-23 20:51:50 -0400

    Open_zwave -> Changing over to use Node number

--- pytomation/interfaces/open_zwave.py
+++ pytomation/interfaces/open_zwave.py
@@ -14,6 +14,7 @@ try:
     from openzwave.option import ZWaveOption
     from openzwave.network import ZWaveNetwork
     #from openzwave.node import ZWaveNode
+    from louie import dispatcher, All
 except:
     print ("Error importing Openzwave and/or Python-Openzwave")
 
@@ -24,19 +25,35 @@ class Open_zwave(HAInterface):
     ready = False
     nodesdisplayed = False
 
+    def louie_network_ready(network):
+	self._logger.info(">>>>>>> Hello from network : I'm ready : %d nodes were found." % self._network.nodes_count)     
+	self._logger.info(">>>>>>> Hello from network : my controller is : %s" % self._network.controller)
+	dispatcher.connect(self.louie_node_update, ZWaveNetwork.SIGNAL_NODE)
+	dispatcher.connect(self.louie_value_update, ZWaveNetwork.SIGNAL_VALUE)
+
+    def louie_node_update(network, node):
+    	self._logger.info('>>>>>>> Hello from node : %s.' % node)
+
+    def louie_value_update(network, node, value):
+    	self._logger.info('>>>>>>> Hello from value : %s.' % value)
+
     def __init__(self, *args, **kwargs):
         self._serialDevicePath = kwargs.get('serialDevicePath', None)
         self._options = ZWaveOption(self._serialDevicePath, \
-          config_path="/usr/share/python-openzwave/config", \
+          config_path="/usr/local/etc/openzwave/", \
           user_path=".", cmd_line="")
         self._options.set_log_file("OZW_Log.log")
         self._options.set_append_log_file(False)
-        self._options.set_console_output(True)
+        self._options.set_console_output(False)
         #self._options.set_save_log_level(log)
         self._options.set_save_log_level('Info')
-        self._options.set_logging(False)
+        self._options.set_logging(True)
+	self._options.set_notify_transactions(True)
         self._options.lock()
-        self._network = ZWaveNetwork(self._options, log=None)
+
+        self._network = ZWaveNetwork(self._options, log=None,autostart=False)
+	dispatcher.connect(self.louie_network_ready, ZWaveNetwork.SIGNAL_NETWORK_READY)
+	self._network.start()
         super(Open_zwave, self).__init__(self, *args, **kwargs)
 
     def _printNetwork(self, node):
@@ -76,6 +93,7 @@ class Open_zwave(HAInterface):
             and not self.ready):
             self.ready = True
             self._logger.info("Network Ready")
+	    self._logger.info("********************************* NETWORK READY ************************************")
         if not self.awake:
             time.sleep(1.0)
             self._logger.debug("Not awaked")
@@ -101,24 +119,28 @@ class Open_zwave(HAInterface):
                           self._network.controller.library_description)
 
     def on(self, address):
-        val = self._network.get_value_from_id_on_network(address)
-        nodeid = val.parent_id
-        if (self._network.nodes[nodeid].set_dimmer(val.value_id, 99)):
-            self._logger.debug('Command dimmer on at' + address)
-        if (self._network.nodes[nodeid].set_switch(val, True)):
-            self._logger.debug('Command switch on at' + address)
+	node = int(address)
+	for val in self._network.nodes[node].get_switches() :
+		self._logger.info("Activate switch")
+		self._network.nodes[node].set_switch(val,True)
+
+	for val in self._network.nodes[node].get_dimmers() :
+		self._logger.info("Activate dimmer : %s" % self._network.nodes[node])
+		self._network.nodes[node].set_dimmer(val,99)
+
 
     def off(self, address):
-        val = self._network.get_value_from_id_on_network(address)
-        nodeid = val.parent_id
-        if (self._network.nodes[nodeid].set_dimmer(val.value_id, 0)):
-            self._logger.debug('Command dimmer off at' + address)
-        if (self._network.nodes[nodeid].set_switch(val, False)):
-            self._logger.debug('Command switch off at' + address)
+	node = int(address)
+	for val in self._network.nodes[node].get_switches() :
+		self._logger.info("Activate switch")
+		self._network.nodes[node].set_switch(val,False)
+
+	for val in self._network.nodes[node].get_dimmers() :
+		self._logger.info("Activate dimmer : %s" % self._network.nodes[node])
+		self._network.nodes[node].set_dimmer(val,0)
 
     def status(self, address):
-        val = self._network.get_value_from_id_on_network(address)
-        nodeid = val.parent_id
-        level = self._network.nodes[nodeid].get_dimmer_level(val)
-        #next lone isn't great
-        self._network.nodes[nodeid].set_dimmer(val.value_id, level)
+	node = int(address)
+	for val in self._network.nodes[node].get_dimmers() :
+	   level = self._network.nodes[nodeid].get_dimmer_level(val)
+
