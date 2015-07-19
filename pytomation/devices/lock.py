a1ccaf418bf513203aa16a156e15eb13d1a462f4
David Heaps <king.dopey.10111@gmail.com>
2015-07-19 01:31:30 -0700

    OpenZwave->Added Level Command for Dimmers OpenZwave->Add more logging OpenZwave->Separate __init__ and _init Lock->Default to always send command (so it will still lock,   even if the device has not sent the unlock state update yet)

--- pytomation/devices/lock.py
+++ pytomation/devices/lock.py
@@ -3,8 +3,9 @@ from pytomation.interfaces import Command
 
 class Lock(InterfaceDevice):
     STATES = [State.UNKNOWN, State.LOCKED, State.UNLOCKED]
-    COMMANDS = [Command.LOCK, Command.UNLOCK, Command.STATUS, Command.TOGGLE]
+    COMMANDS = [Command.LOCK, Command.UNLOCK, Command.TOGGLE, Command.STATUS]
     
     def _initial_vars(self, *args, **kwargs):
         super(Lock, self)._initial_vars(*args, **kwargs)
         self._restricted = False
+        self._send_always = True
--- pytomation/interfaces/open_zwave.py
+++ pytomation/interfaces/open_zwave.py
@@ -21,10 +21,7 @@ except:
 
 
 class Open_zwave(HAInterface):
-    VERSION = '0.0.3'
-    awake = False
-    ready = False
-    nodesdisplayed = False
+    VERSION = '0.0.4'
 
     def louie_network_ready(self, network):
         self._logger.info(">>>>>>> Hello from network : I'm ready : %d nodes were found.".format(self._network.nodes_count))
@@ -40,29 +37,36 @@ class Open_zwave(HAInterface):
         for lockvalue in self.get_door_locks(node.node_id).values():
             if lockvalue.value_id == value.value_id:
                 if value.data:
-                    self._onCommand(address=str(node.node_id), command=Command.LOCK)
+                    self._onCommand(address=node.node_id, command=Command.LOCK)
                 else:
-                    self._onCommand(address=str(node.node_id), command=Command.UNLOCK)
+                    self._onCommand(address=node.node_id, command=Command.UNLOCK)
         for val in self._network.nodes[node.node_id].get_switches():
             if val.value_id == value.value_id:
                 level = value.data
                 if level:
-                    self._onCommand(address=str(node.node_id), command=Command.ON)
+                    self._onCommand(address=node.node_id, command=Command.ON)
                 else:
-                    self._onCommand(address=str(node.node_id), command=Command.OFF)
+                    self._onCommand(address=node.node_id, command=Command.OFF)
         for val in self._network.nodes[node.node_id].get_dimmers() :
             level = self._network.nodes[node].get_dimmer_level(val)
             if level < 2:
-                self._onCommand(address=str(node.node_id), command=Command.OFF)
+                self._onCommand(address=node.node_id, command=Command.OFF)
             elif level > 98:
-                self._onCommand(address=str(node.node_id), command=Command.ON)
+                self._onCommand(address=node.node_id, command=Command.ON)
             else:
-                self._onCommand(address=str(node.node_id), command=(Command.LEVEL,level))
+                self._onCommand(address=node.node_id, command=(Command.LEVEL,level))
 
     def __init__(self, *args, **kwargs):
         self._serialDevicePath = kwargs.get('serialDevicePath', None)
+        self._configpath = kwargs.get('config_path', "/etc/openzwave/")
+        super(Open_zwave, self).__init__(self, *args, **kwargs)
+
+    def _init(self, *args, **kwargs):
+        self.awake = False
+        self.ready = False
+        self.nodesdisplayed = False
         self._options = ZWaveOption(self._serialDevicePath, \
-          config_path=kwargs.get('config_path', "/etc/openzwave/"), \
+          config_path=self._configpath, \
           user_path=".", cmd_line="")
         self._options.set_log_file("OZW_Log.log")
         self._options.set_append_log_file(False)
@@ -76,37 +80,34 @@ class Open_zwave(HAInterface):
         self._network = ZWaveNetwork(self._options, log=None,autostart=False)
         dispatcher.connect(self.louie_network_ready, ZWaveNetwork.SIGNAL_NETWORK_READY)
         self._network.start()
-        super(Open_zwave, self).__init__(self, *args, **kwargs)
+        super(Open_zwave, self)._init(self, *args, **kwargs)
 
     def _printNetwork(self, node):
         node = self._network.nodes[node]
-        self._logger.debug("------------------------------------------------------")
-        self._logger.debug("{} - Name : {}".format(node.node_id,
+        self._logger.info("------------------------------------------------------")
+        self._logger.info("{} - Name : {}".format(node.node_id,
             node.name))
-        self._logger.debug("{} - Manufacturer name / id : {} / {}".format(
+        self._logger.info("{} - Manufacturer name / id : {} / {}".format(
             node.node_id,
             node.manufacturer_name,
             node.manufacturer_id))
-        self._logger.debug("{} - Product name / id / type : {} / {} / {}".format(
+        self._logger.info("{} - Product name / id / type : {} / {} / {}".format(
             node.node_id,
             node.product_name,
             node.product_id,
             node.product_type))
-        self._logger.debug("{} - Version : {}".format(node.node_id,
+        self._logger.info("{} - Version : {}".format(node.node_id,
             node.version))
-        self._logger.debug("{} - Command classes : {}".format(node.node_id,
+        self._logger.info("{} - Command classes : {}".format(node.node_id,
             node.command_classes_as_string))
-        self._logger.debug("{} - Capabilities : {}".format(node.node_id,
+        self._logger.info("{} - Capabilities : {}".format(node.node_id,
             node.capabilities))
-        self._logger.debug("{} - Neighbors : {}".format(node.node_id,
+        self._logger.info("{} - Neighbors : {}".format(node.node_id,
             node.neighbors))
-        self._logger.debug("{} - Can sleep : {}".format(node.node_id,
+        self._logger.info("{} - Can sleep : {}".format(node.node_id,
             node.can_wake_up()))
-        for value in self.get_door_locks(node.node_id).values():
+        for value in self.get_door_locks(node.node_id, 'All').values():
             self._logger.debug("{} - {} : {}".format(node.node_id,value.label,value.data))
-        
-    def _init(self, *args, **kwargs):
-        super(Open_zwave, self)._init(self, *args, **kwargs)
 
     def _readInterface(self, lastPacketHash):
         if (self._network.state >= self._network.STATE_AWAKED
@@ -118,14 +119,10 @@ class Open_zwave(HAInterface):
             self.ready = True
             self._logger.info("Network Ready")
         if not self.awake:
-            time.sleep(1.0)
             self._logger.debug("Not awaked")
-            return
-        if self.awake and not self.ready:
-            time.sleep(1.0)
+        elif self.awake and not self.ready:
             self._logger.debug("Not ready")
-            return
-        if not self.nodesdisplayed and self.ready:
+        elif not self.nodesdisplayed and self.ready:
             for node in self._network.nodes:
                 self._printNetwork(node)
             self.update_status()
@@ -139,9 +136,9 @@ class Open_zwave(HAInterface):
         self._logger.info("Use python library : {}".format(self._network.controller.python_library_version))
         self._logger.info("Use ZWave library : {}".format(self._network.controller.library_description))
         
-    def get_door_locks(self, node):
+    def get_door_locks(self, node, datatype = 'Bool'):
         return self._network.nodes[node].get_values(class_id=0x62, genre='User', \
-            type='Bool', readonly=False, writeonly=False)
+            type=datatype, readonly=False, writeonly=False)
         
     def lock(self, address):
         node = int(address)
@@ -168,12 +165,18 @@ class Open_zwave(HAInterface):
     def off(self, address):
         node = int(address)
         for val in self._network.nodes[node].get_switches() :
-            self._logger.debug("Activate switch")
+            self._logger.debug("Deactivate switch")
             self._network.nodes[node].set_switch(val,False)
 
         for val in self._network.nodes[node].get_dimmers() :
-            self._logger.debug("Activate dimmer : {}".format(self._network.nodes[node]))
+            self._logger.debug("Deactivate dimmer : {}".format(self._network.nodes[node]))
             self._network.nodes[node].set_dimmer(val,0)
+
+    def level(self, address, level):
+        node = int(address)
+        for val in self._network.nodes[node].get_dimmers() :
+            self._logger.debug("Set dimmer : {}".format(self._network.nodes[node]))
+            self._network.nodes[node].set_dimmer(val, level)
 
     def status(self, address):
         node = int(address)
