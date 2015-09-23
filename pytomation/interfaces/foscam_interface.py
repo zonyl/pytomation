7c702e232cc79291f3e400134814ee725c57c68e
texnofobix <texnofobix@gmail.com>
2015-09-22 23:50:03 -0400

    foscam_interface -> reducing blocking of readInterface

--- pytomation/interfaces/foscam_interface.py
+++ pytomation/interfaces/foscam_interface.py
@@ -10,6 +10,9 @@ class Foscam(HAInterface):
     MOTIONDETECTION_ON = '1'
     MOTIONDETECTION_ALERT = '2'
 
+    _iteration = 0
+    _poll_secs = 15
+
     def __init__(self, *args, **kwargs):
         super(Foscam, self).__init__(None, *args, **kwargs)
 
@@ -22,18 +25,19 @@ class Foscam(HAInterface):
         self._interface = FoscamCamera(host=self._host, port=self._port, usr=self._username, pwd=self._password, verbose=False)
 
     def _readInterface(self, lastPacketHash):
-        motion = self._interface.get_dev_state()[1]['motionDetectAlarm'][0]
+	if not self._iteration < self._poll_secs:
+		self._iteration = 0
+		motion = self._interface.get_dev_state()[1]['motionDetectAlarm'][0]
 
-        if motion == self.MOTIONDETECTION_ALERT:
-            self._onCommand(command=Command.MOTION, address=self._host)
+		if motion == self.MOTIONDETECTION_ALERT:
+		    self._onCommand(command=Command.MOTION, address=self._host)
 
-        if motion == self.MOTIONDETECTION_ON:
-            self._onCommand(command=(Command.STILL))
+		if motion == self.MOTIONDETECTION_ON:
+		    self._onCommand(command=(Command.STILL))
 
-        if motion == self.MOTIONDETECTION_OFF:
-            self._onCommand(command=(Command.OFF))
-            time.sleep(5)
-
+		if motion == self.MOTIONDETECTION_OFF:
+		    self._onCommand(command=(Command.OFF))
+	self._iteration += 1
         time.sleep(1)
 
     def activate(self, *args, **kwargs):
